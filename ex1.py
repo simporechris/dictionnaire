90f7fe339030afd74400d1e8259621a83bd9a7aa
simporechris <2468242@cegepoutaouais.qc.ca>
2025-10-09 16:31:17 -0400

    k

---
+++ ex1.py
@@ -0,0 +1,9 @@
+ls_epicerie = ["Pomme","Pain","Lait","Riz","Tomate","Œufs","Beurre","Fromage","Pâtes","Café"]
+print(ls_epicerie.sort())
+
+dict_jeux = {
+    "titre": "Fortnite",
+    "description": "Un jeu de bataille royale en ligne où 100 joueurs s'affrontent pour être le dernier survivant.",
+    "prix": 0,
+}
+print(dict_jeux)
